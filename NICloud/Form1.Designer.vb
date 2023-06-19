3d7124ca5e6aa6e9a58dfa1f15c89f9fcb8cebbf
Mtrx000 <mtrxsrv@gmail.com>
2023-06-19 10:33:05 +0200

    update

---
+++ NICloud/Form1.Designer.vb
@@ -0,0 +1,136 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form remplace la méthode Dispose pour nettoyer la liste des composants.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Requise par le Concepteur Windows Form
+    Private components As System.ComponentModel.IContainer
+
+    'REMARQUE : la procédure suivante est requise par le Concepteur Windows Form
+    'Elle peut être modifiée à l'aide du Concepteur Windows Form.  
+    'Ne la modifiez pas à l'aide de l'éditeur de code.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.ListBox1 = New System.Windows.Forms.ListBox()
+        Me.Button2 = New System.Windows.Forms.Button()
+        Me.Button3 = New System.Windows.Forms.Button()
+        Me.Button4 = New System.Windows.Forms.Button()
+        Me.Button5 = New System.Windows.Forms.Button()
+        Me.Button6 = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'Button1
+        '
+        Me.Button1.BackColor = System.Drawing.Color.DarkGray
+        Me.Button1.Font = New System.Drawing.Font("Arial", 14.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Button1.Location = New System.Drawing.Point(292, 12)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(204, 50)
+        Me.Button1.TabIndex = 0
+        Me.Button1.Text = "Import a File (< 1GB)"
+        Me.Button1.UseVisualStyleBackColor = False
+        '
+        'ListBox1
+        '
+        Me.ListBox1.BackColor = System.Drawing.Color.FromArgb(CType(CType(64, Byte), Integer), CType(CType(64, Byte), Integer), CType(CType(64, Byte), Integer))
+        Me.ListBox1.ForeColor = System.Drawing.Color.White
+        Me.ListBox1.FormattingEnabled = True
+        Me.ListBox1.Location = New System.Drawing.Point(12, 5)
+        Me.ListBox1.Name = "ListBox1"
+        Me.ListBox1.Size = New System.Drawing.Size(146, 264)
+        Me.ListBox1.TabIndex = 1
+        '
+        'Button2
+        '
+        Me.Button2.BackColor = System.Drawing.Color.DarkGray
+        Me.Button2.Font = New System.Drawing.Font("Arial", 14.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Button2.Location = New System.Drawing.Point(292, 68)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(204, 50)
+        Me.Button2.TabIndex = 2
+        Me.Button2.Text = "Download"
+        Me.Button2.UseVisualStyleBackColor = False
+        '
+        'Button3
+        '
+        Me.Button3.BackColor = System.Drawing.Color.DarkGray
+        Me.Button3.Font = New System.Drawing.Font("Arial", 14.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Button3.Location = New System.Drawing.Point(292, 124)
+        Me.Button3.Name = "Button3"
+        Me.Button3.Size = New System.Drawing.Size(204, 50)
+        Me.Button3.TabIndex = 3
+        Me.Button3.Text = "Update"
+        Me.Button3.UseVisualStyleBackColor = False
+        '
+        'Button4
+        '
+        Me.Button4.BackColor = System.Drawing.Color.DarkGray
+        Me.Button4.Font = New System.Drawing.Font("Arial", 14.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Button4.Location = New System.Drawing.Point(292, 180)
+        Me.Button4.Name = "Button4"
+        Me.Button4.Size = New System.Drawing.Size(204, 50)
+        Me.Button4.TabIndex = 4
+        Me.Button4.Text = "Close"
+        Me.Button4.UseVisualStyleBackColor = False
+        '
+        'Button5
+        '
+        Me.Button5.Location = New System.Drawing.Point(292, 233)
+        Me.Button5.Name = "Button5"
+        Me.Button5.Size = New System.Drawing.Size(93, 34)
+        Me.Button5.TabIndex = 5
+        Me.Button5.Text = "Border ON"
+        Me.Button5.UseVisualStyleBackColor = True
+        '
+        'Button6
+        '
+        Me.Button6.Location = New System.Drawing.Point(403, 233)
+        Me.Button6.Name = "Button6"
+        Me.Button6.Size = New System.Drawing.Size(93, 34)
+        Me.Button6.TabIndex = 6
+        Me.Button6.Text = "Border OFF"
+        Me.Button6.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackColor = System.Drawing.Color.Silver
+        Me.ClientSize = New System.Drawing.Size(555, 279)
+        Me.Controls.Add(Me.Button6)
+        Me.Controls.Add(Me.Button5)
+        Me.Controls.Add(Me.Button4)
+        Me.Controls.Add(Me.Button3)
+        Me.Controls.Add(Me.Button2)
+        Me.Controls.Add(Me.ListBox1)
+        Me.Controls.Add(Me.Button1)
+        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None
+        Me.MaximizeBox = False
+        Me.Name = "Form1"
+        Me.ShowIcon = False
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
+        Me.Text = "TempCloud - FTP File Cloud"
+        Me.ResumeLayout(False)
+
+    End Sub
+
+    Friend WithEvents Button1 As Button
+    Friend WithEvents ListBox1 As ListBox
+    Friend WithEvents Button2 As Button
+    Friend WithEvents Button3 As Button
+    Friend WithEvents Button4 As Button
+    Friend WithEvents Button5 As Button
+    Friend WithEvents Button6 As Button
+End Class
---
+++ NICloud/Form1.vb
@@ -0,0 +1,214 @@
+﻿Imports System.IO
+Imports System.Net
+Imports System.Windows.Forms
+
+Public Class Form1
+    Private ftpServer As String = "ftp://exemple.com/"
+    Private ftpUsername As String = "username"
+    Private ftpPassword As String = "password"
+    Private limit As Long = 10 'GB Limit
+
+    Private ftpDirectory As String ' FTP Folder
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Dim openFileDialog As New OpenFileDialog()
+        openFileDialog.Filter = "All Files (*.*)|*.*"
+        openFileDialog.RestoreDirectory = True
+
+        If openFileDialog.ShowDialog() = DialogResult.OK Then
+
+            Dim fileSize As Long = New FileInfo(openFileDialog.FileName).Length
+            Dim maxSize As Long = 1024 * 1024 * 1024
+
+            If fileSize > maxSize Then
+                MessageBox.Show("This file exceeds the limit (1GB)", "Error")
+            Else
+
+                Dim fileExtension As String = Path.GetExtension(openFileDialog.FileName)
+                If fileExtension = ".exe" OrElse fileExtension = ".htm" OrElse fileExtension = ".html" OrElse fileExtension = ".php" OrElse fileExtension = ".css" OrElse fileExtension = ".py" OrElse fileExtension = ".pyw" Then
+                    MessageBox.Show("The .exe .htm .html .php .css .py .pyw files is not allowed ", "Error")
+                Else
+
+                    UploadFileToFtp(openFileDialog.FileName)
+                    MessageBox.Show("The file has been sent")
+                End If
+            End If
+        End If
+    End Sub
+
+    Private Sub UploadFileToFtp(filePath As String)
+        Dim fileName As String = Path.GetFileName(filePath)
+        Dim request As FtpWebRequest = DirectCast(WebRequest.Create(Path.Combine(ftpDirectory, fileName)), FtpWebRequest)
+        request.Method = WebRequestMethods.Ftp.UploadFile
+        request.Credentials = New NetworkCredential(ftpUsername, ftpPassword)
+
+        Dim fileContents As Byte() = File.ReadAllBytes(filePath)
+        request.ContentLength = fileContents.Length
+
+        Using requestStream As Stream = request.GetRequestStream()
+            requestStream.Write(fileContents, 0, fileContents.Length)
+            requestStream.Close()
+        End Using
+    End Sub
+
+    Private Sub RefreshFileList()
+        ListBox1.Items.Clear()
+
+        Dim request As FtpWebRequest = WebRequest.Create(ftpDirectory)
+        request.Method = WebRequestMethods.Ftp.ListDirectory
+
+        request.Credentials = New NetworkCredential(ftpUsername, ftpPassword)
+
+        Dim response As FtpWebResponse = request.GetResponse()
+        Dim responseStream As Stream = response.GetResponseStream()
+        Dim reader As New StreamReader(responseStream)
+
+        While Not reader.EndOfStream
+            ListBox1.Items.Add(reader.ReadLine())
+        End While
+
+        reader.Close()
+        response.Close()
+    End Sub
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+
+        Try
+            If ListBox1.SelectedItem IsNot Nothing Then
+                Dim selectedFile As String = ListBox1.SelectedItem.ToString()
+                Dim desktopPath As String = Environment.GetFolderPath(Environment.SpecialFolder.Desktop)
+                Dim localFilePath As String = Path.Combine(desktopPath, selectedFile)
+
+                Dim request As FtpWebRequest = WebRequest.Create(Path.Combine(ftpDirectory, selectedFile))
+                request.Method = WebRequestMethods.Ftp.DownloadFile
+
+                request.Credentials = New NetworkCredential(ftpUsername, ftpPassword)
+
+                Dim response As FtpWebResponse = request.GetResponse()
+                Dim responseStream As Stream = response.GetResponseStream()
+                Dim fileStream As New FileStream(localFilePath, FileMode.Create)
+
+                responseStream.CopyTo(fileStream)
+
+                fileStream.Close()
+                response.Close()
+
+
+                DeleteFileFromFtp(selectedFile)
+                MessageBox.Show("The File has been downloaded")
+            Else
+                MessageBox.Show("Please select a file")
+            End If
+        Catch
+            MessageBox.Show("Error, Please Update", "Error")
+        End Try
+
+    End Sub
+
+    Private Sub DeleteFileFromFtp(fileName As String)
+        Dim request As FtpWebRequest = WebRequest.Create(Path.Combine(ftpDirectory, fileName))
+        request.Method = WebRequestMethods.Ftp.DeleteFile
+        request.Credentials = New NetworkCredential(ftpUsername, ftpPassword)
+
+        Dim response As FtpWebResponse = request.GetResponse()
+        response.Close()
+    End Sub
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        CreateFtpDirectory()
+        RefreshFileList()
+    End Sub
+
+    Private Sub CreateFtpDirectory()
+        Dim random As New Random()
+        Dim folderName As String = GenerateRandomName(20)
+        ftpDirectory = ftpServer & folderName
+
+        Dim request As FtpWebRequest = WebRequest.Create(ftpDirectory)
+        request.Method = WebRequestMethods.Ftp.MakeDirectory
+        request.Credentials = New NetworkCredential(ftpUsername, ftpPassword)
+
+        Try
+            Dim response As FtpWebResponse = request.GetResponse()
+            response.Close()
+        Catch ex As WebException
+
+            Me.Hide()
+            MessageBox.Show("Error")
+            Me.Close()
+        End Try
+    End Sub
+
+    Private Sub DeleteFtpDirectory()
+        Dim request As FtpWebRequest = WebRequest.Create(ftpDirectory)
+        request.Method = WebRequestMethods.Ftp.RemoveDirectory
+        request.Credentials = New NetworkCredential(ftpUsername, ftpPassword)
+
+        Dim response As FtpWebResponse = Nothing
+
+        Try
+            response = request.GetResponse()
+            response.Close()
+        Catch ex As WebException
+            Me.Close()
+        End Try
+    End Sub
+
+    Private Sub CheckTotalFileSize()
+        Dim request As FtpWebRequest = WebRequest.Create(ftpServer)
+        request.Method = WebRequestMethods.Ftp.ListDirectoryDetails
+        request.Credentials = New NetworkCredential(ftpUsername, ftpPassword)
+
+        Dim response As FtpWebResponse = request.GetResponse()
+        Dim responseStream As Stream = response.GetResponseStream()
+        Dim reader As New StreamReader(responseStream)
+
+        Dim totalSize As Long = 0
+
+        While Not reader.EndOfStream
+            Dim line As String = reader.ReadLine()
+            Dim lineSplit As String() = line.Split(New Char() {" "c}, StringSplitOptions.RemoveEmptyEntries)
+
+            Dim fileSize As Long = 0
+            Long.TryParse(lineSplit(4), fileSize)
+
+            totalSize += fileSize
+        End While
+
+        reader.Close()
+        response.Close()
+
+        Dim limitSizeInBytes As Long = limit * 1024 * 1024 * 1024
+
+        If totalSize > limitSizeInBytes Then
+            Me.Close()
+        End If
+    End Sub
+
+    Private Function GenerateRandomName(length As Integer) As String
+        Const chars As String = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789"
+        Dim random As New Random()
+        Return New String(Enumerable.Repeat(chars, length).Select(Function(s) s(random.Next(s.Length))).ToArray())
+    End Function
+
+    Private Sub Button3_Click(sender As Object, e As EventArgs) Handles Button3.Click
+        RefreshFileList()
+    End Sub
+
+    Private Sub Button5_Click(sender As Object, e As EventArgs) Handles Button5.Click
+        Me.FormBorderStyle = FormBorderStyle.FixedSingle
+    End Sub
+
+    Private Sub Button6_Click(sender As Object, e As EventArgs) Handles Button6.Click
+        Me.FormBorderStyle = FormBorderStyle.None
+    End Sub
+
+    Private Sub Button4_Click(sender As Object, e As EventArgs) Handles Button4.Click
+        Try
+            DeleteFtpDirectory()
+        Catch
+            Me.Close()
+        End Try
+        Me.Close()
+    End Sub
+End Class
